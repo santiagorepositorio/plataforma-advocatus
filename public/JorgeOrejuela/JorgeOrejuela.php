faccad1d20528dab48bf091d3ab08abf1aa7dd4d
Jorge Orejuela <31361765+jorgeorejuela@users.noreply.github.com>
2023-09-15 20:42:01 -0500

    Carpeta Public Jorge Orejuela
    Creación de Carpeta de Prueba de confirmación de clonación del proyecto

---
+++ public/JorgeOrejuela/JorgeOrejuela.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jorge Orejuela</title>
+</head>
+
+<body>
+
+    <h1>Hola desde mi pagina de Inicio. Jorge Orejuela</h1>
+
+</body>
+
+</html>
